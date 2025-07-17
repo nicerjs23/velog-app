9e4d8ea44f3c2740e5b6384a3028e6cf0ec82d12
nicerjs23 <nicerjs23@gmail.com>
2025-07-17 18:56:35 +0900

    Feat: ìcors경로 51경로추가

--- src/main/java/com/example/velog_app/controller/BoardController.java
+++ src/main/java/com/example/velog_app/controller/BoardController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import com.example.velog_app.dto.BoardResponseDto;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins="http://localhost:5173")
+@CrossOrigin(origins={"http://localhost:5173","http://localhost:5174"})
 @RestController
 public class BoardController {
     private final BoardRepository boardRepository;
